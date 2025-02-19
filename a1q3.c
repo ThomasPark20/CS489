69b84918eb67dfdfb4dfaedf36c06c32ad79f6e6
Thomas Park <55768211+ThomasPark20@users.noreply.github.com>
2025-02-19 12:52:41 -0800

    Create a1q3.c

---
+++ a1q3.c
@@ -0,0 +1,18 @@
+#define BUFFSIZE 256
+    
+int read_file(const char *filename) {
+    FILE* ptr = fopen(filename, "r");
+    
+    if (ptr == NULL) {
+        printf("File cannot be opened.\n");
+        return(-1);
+    }
+    
+    char buff[BUFFSIZE];
+    
+    while (fgets(buff, BUFFSIZE, ptr) != NULL) {
+        printf("%s", buff);
+    }
+        
+    return 0; 
+}
